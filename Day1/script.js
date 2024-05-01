8022fb18d9a461d48d04ee1db0fe9658e591b8ec
SaggyintoIT <Kumarsagar500@gmail.com>
2024-05-01 13:04:28 +0530

    Adding Day2

--- Day1/script.js
+++ Day1/script.js
@@ -2,4 +2,4 @@ const Heading = ()=>{
     return <h1>Hello Geekster</h1>
 
 }
- ReactDOM.render(<Heading/>, document.getElementById('root')   );
+ ReactDOM.render(<Heading/>, document.getElementById('roots')   );
